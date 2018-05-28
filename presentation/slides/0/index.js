764ba4e5460c20f20550c6f8f0616ebf4eac7746
Derek Clair <derek@derekclair.com>
2018-05-28 14:03:42 -0600

    Title Slide

--- presentation/slides/0/index.js
+++ presentation/slides/0/index.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { Fit, Heading, Image, Layout, Slide } from 'spectacle';
+import {
+  Fill,
+  Fit,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  Slide,
+  Text,
+} from 'spectacle';
 
 import images from '../images';
 
 /** ************************************************************************* */
 
 export default (
-  <Slide transition={['zoom']} bgColor="primary">
-    <Heading size={1} fit lineHeight={1} textColor="black">
+  <Slide
+    id="title"
+    goTo={2}
+    transition={['zoom']}
+    bgColor="white"
+    bgImage={images.react.replace('/', '')}
+    bgSize="600px"
+    bgPostition="center"
+    bgRepeat="no-repeat"
+  >
+    <Heading caps size={1} lineHeight={1} textColor="black">
       What&apos;s new
     </Heading>
-    <Fit>
-      <Image
-        src={images.react.replace('/', '')}
-        margin="0px auto 40px"
-      />
-    </Fit>
-
-    <Heading size={1} fit>
-      React v16.3 <small>(&amp; v16.4)</small>
+    <Heading bold caps size={4} textColor="black">
+      in
     </Heading>
+    <Heading fit size={1} textColor="title">
+      React 16
+    </Heading>
+    <Text lineHeight="1em">(v16.3 &amp; v16.4)</Text>
+    <Link
+      href="https://github.com/basementlab/meetup-react-2018-05-30"
+      target="_blank"
+    >
+      <Text
+        caps
+        fit
+        size={4}
+        textColor="black"
+        textFont="Fira Code"
+        // textSize="1em"
+        lineHeight="1em"
+      >
+        <Image
+          src={images.github.replace('/', '')}
+          margin="0 0 -0.5em 0"
+          height={'2em'}
+          display="inline"
+        />
+        {'/basementlab/meetup-react-2018-05-30'}
+      </Text>
+    </Link>
   </Slide>
 );
